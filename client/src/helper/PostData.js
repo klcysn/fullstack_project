da9679b63df513d7297ec515c01d4191b9a8460b
klcysn <klcysn@users.noreply.github.com>
2020-12-14 20:41:05 +0100

    Fix login bugs

--- client/src/helper/PostData.js
+++ client/src/helper/PostData.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const postData = async (path, data) => {
   
-  const token = localStorage.getItem("token");
-  console.log("token", token);
+  // const token = localStorage.getItem("token");
+  // console.log("token", token);
+  const token = "token";
   const response = await axios.post(`${path}`, data, {
     headers: {
       token,
